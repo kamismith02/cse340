16da08977ffe3edf1fc77a51e0164b42a97092ce
kamismith02 <kamilou0709@gmail.com>
2024-05-23 19:45:06 -0600

    registering

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -6,9 +6,13 @@ const utilities = require("../utilities/index");
 
 router.get('/login', utilities.handleErrors(accountController.buildLogin));
 
+router.get('/register', utilities.handleErrors(accountController.buildRegister));
+
 router.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
+
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router;
--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const errorHandler = require("./routes/errorRoute");
 const session = require("express-session")
 const pool = require('./database/')
 const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
@@ -39,6 +40,9 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Templates
